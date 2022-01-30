643b0fab0b7ed19afbf50123cdfbe65e5e13629c
tiamabderezai <pass>
2022-01-30 15:38:49 -0500

    Added MainActivity.kt

--- app/src/main/java/com/example/benachrichtigungen/MainActivity.kt
+++ app/src/main/java/com/example/benachrichtigungen/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.benachrichtigungen
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.benachrichtigungen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val name = "Notif Channel"
+        val desc = "A description of the Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(1,name, importance)
+        channel.description = desc
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
