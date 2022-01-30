332e8bed664375f79a27a3df2eb51a9eb2c50d4a
tiamabderezai <pass>
2022-01-30 16:07:55 -0500

    Completed

--- app/src/main/java/com/example/benachrichtigungen/MainActivity.kt
+++ app/src/main/java/com/example/benachrichtigungen/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.benachrichtigungen
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.app.*
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.benachrichtigungen.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +16,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
+
         createNotificationChannel()
+        _binding.submitButton.setOnClickListener { scheduleNotification() }
     }
+
+    private fun scheduleNotification() {
+        val intent = Intent(applicationContext, Notification::class.java)
+        val title = _binding.titleET.text.toString()
+        val message = _binding.messageET.text.toString()
+        intent.apply {
+            putExtra(titleExtra,title)
+            putExtra(messageExtra,message)
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            notificationID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val time = getTime()
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            time,
+            pendingIntent
+        )
+        showAlert(time,title,message)
+    }
+
+    private fun showAlert(time: Long, title: String, message: String) {
+        val date = Date(time)
+        val dateFormat = android.text.format.DateFormat.getLongDateFormat(applicationContext)
+        val timeFormat = android.text.format.DateFormat.getTimeFormat(applicationContext)
+
+        AlertDialog.Builder(this)
+            .setTitle("Notification Scheduled")
+            .setMessage(
+                "Title: " + title +
+                        "\nMessage: " + message +
+                        "\nAt: " + dateFormat.format(date) + " " + timeFormat.format(date))
+            .setPositiveButton("Okay"){_,_ ->}
+            .show()
+    }
+
+    private fun getTime(): Long {
+        val minute = _binding.timePicker.minute
+        val hour = _binding.timePicker.hour
+        val day = _binding.datePicker.dayOfMonth
+        val month = _binding.datePicker.month
+        val year = _binding.datePicker.year
+
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day, hour, minute)
+        return calendar.timeInMillis
+    }
+
 
     private fun createNotificationChannel() {
         val name = "Notif Channel"
